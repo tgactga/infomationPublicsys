08c2770fcc768c8e81e63cd00cc84d27659923cc
tgactga@126.com <huangwei9253081056>
2015-12-18 11:31:36 +0800

    zh

---
+++ infomationPublicsys/YaoJiaForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace infomationPublicsys
+{
+    public partial class YaoJiaForm : Form
+    {
+        public YaoJiaForm()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void YaoJiaForm_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(7680, 7680);    
+        }
+    }
+}
